8f890a339d4b1166d6d99c2982e97ab721fd5bec
oob3003 <bruno.gottini@free.fr>
2020-11-29 13:08:01 +0100

    connexion serveur Mongo OK

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+let url = "http://localhost:3000/api/teddies";
+
+fetch(url).then(function (response) {
+    response.json().then(function (teddy) {
+        for(let teddy in teddies ) {teddy};
+    });
+});
+/* request.onload = function() {
+  var teddyChoose = request.response;
+  populateHeader(teddyChoose);
+  showHeroes(teddyChoose);
+}  */
+function afficher2(image){
+    $('#div_display2').html('<img src="' + image + '" >'); // tu avais oublié le "_"
+dans le nom de la div
+    $('#div_display2').fadeIn(); // Inversion du html et du fadeIn
+}
+
 var teddyChoose = document.querySelector('select');
-var colorDisplay = document.querySelector('pre');
 
 teddyChoose.onchange = function () {
     var teddy = teddyChoose.value;
     updateDisplay(teddy);
 };
 
-function updateDisplay(teddy) {
+/*function updateDisplay(teddy) {
     teddy = teddy.replace("_", " ");  
     teddy = teddy.toLowerCase();
-    var url = http://localhost:3000/api/teddies;
-
-    fetch(url).then(function (response) {
-        response.text().then(function (text) {
-            colorDisplay.textContent = text;
-        });
-    });
-};
+    
+    
+};*/
