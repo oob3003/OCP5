217147ee8b934ffa9fe424b35f4bfeca53c9b01e
oob3003 <bruno.gottini@free.fr>
2020-12-16 18:42:12 +0100

    débogage appel au serveur

--- index.js
+++ index.js
@@ -4,14 +4,87 @@ fetch(url)
   .then(function(response) {
     response.json()
     .then(function(data){
-      data.forEach(element => {
-        console.log(element);
-      })
+      parseTeddies(data)
     })
   })
+.then(response => {})
+.catch(error => alert("Erreur : " + error));
+
+function parseTeddies(teddies) {
+  let teddyDisplayerElement = document.getElementById('teddyDisplayer')
+  let htmlToCreate = ''
+
+  for(let index = 0; index < teddies.length; index++) {
+    let teddy = teddies[index]
+    htmlToCreate += '<div>'
+    htmlToCreate += teddy.name + ' '
+    htmlToCreate += teddy.description + ' '
+    htmlToCreate += '</div>'
+  }
+
+  teddyDisplayerElement.innerHTML = htmlToCreate
+}  
+
+/*  
+let index = 0;
+const teddy1 = 0 ;
+const teddiesList = [0,1,2,3,4,];
+  
+for (let i = 0; i < teddiesList.length; i++) {
+    if (teddy1 === teddiesList[i]) {
+        index = i;
+    }
+}
+*/
+/* créer une requête pour que la div .photo-teddy1 permette l'affichage de la photo du 1er ourson */
+/*
+document.querySelector('#btnAfficher').addEventListener('click', function() {
+	document.querySelector('.photo-teddy1').innerHTML='emplacement photo';
+})
+*/
 
 
-    
+/*
+document.querySelector('#btnAfficher').addEventListener('click', function() {
+	document.querySelector('#infosTeddies').innerHTML='toujours pas ça!';
+})
+*/
+
+/*
+for(var i=1;i<7;i++) {
+	document.querySelector('#couleur'+i).addEventListener('click', function() {
+		document.querySelector('#page').style.backgroundColor=this.style.backgroundColor;
+	})
+}
+/*
+function TeddiesList (element) {
+  let name = document.createPortfolio("article");
+  name.innerHTML = element.name;
+};
+
+
+let teddy = {
+  colors : [''],
+  description : '',
+  imageUrl : '',
+  name : '',
+  price :  
+};
+
+let teddyColors = teddy.colors;
+let teddyDescription = teddy.description;
+
+
+document.querySelector('#portfolio').innerText = 
+name : ${teddy.name}
+price : ${teddy.price} €
+
+
+/* let elt = document.getElementByClassName('portfolio__photos');
+const articles = document.getElementsByTagName('article');
+const thirdArticle = articles[0];
+elt.innerHTML = "<ul><li><article>Norbert</article></li></ul>";
+*/  
 /*function requete(images) {
   var portfolio = document.getElementById("portfolio")
 
